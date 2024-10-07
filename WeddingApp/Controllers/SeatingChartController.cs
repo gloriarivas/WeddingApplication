b4ce6d05432a5e576466228896eb75254ae5618c
Gloria Rivas-Bonilla <Grivas8325@conestogac.on.ca>
2024-10-07 13:53:33 -0400

    seating chart init

--- WeddingApp/Controllers/SeatingChartController.cs
+++ WeddingApp/Controllers/SeatingChartController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WeddingApp.DataAccess;
+using WeddingApp.Models;
+using WeddingAppDatabase.Entities;
 
 namespace WeddingApp.Controllers
 {
+
     public class SeatingChartController : Controller
     {
+        private WeddingDbContext _weddingDbContext;
+        public SeatingChartController(WeddingDbContext weddingDbContext)
+        {
+            _weddingDbContext = weddingDbContext;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult GetSeatingChart()
+        {
+            List<SeatingChart> seats = _weddingDbContext.SeatingChart.ToList();
+            List<Tables> tables = _weddingDbContext.Tables.ToList();
+            SeatingChartViewModel seatingChart = new SeatingChartViewModel()
+            {
+                Seats = seats,
+                Tables = tables
+            };
+            return View();
+
+        }
     }
+    
 }
---
+++ WeddingApp/Models/SeatingChartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WeddingAppDatabase.Entities;
+
+namespace WeddingApp.Models
+{
+    public class SeatingChartViewModel
+    {
+        public List<Tables> Tables { get; set; }
+        public List<SeatingChart> Seats { get; set; }
+    }
+}
