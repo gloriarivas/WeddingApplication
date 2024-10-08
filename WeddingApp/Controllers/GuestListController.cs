00fba2d3c673aeb37c3c3df99140adbdbb769160
Gloria Rivas-Bonilla <Grivas8325@conestogac.on.ca>
2024-10-08 10:24:34 -0400

    buttons for edit and delete

--- WeddingApp/Controllers/GuestListController.cs
+++ WeddingApp/Controllers/GuestListController.cs
@@ -23,31 +23,57 @@ namespace WeddingApp.Controllers
             return View() ;
         }
 
+        /// <summary>
+        /// Get req for add guest, goes to a blank add page
+        /// </summary>
+        /// <returns></returns>
         [HttpGet()]
         public IActionResult AddNewGuestRequest()
         {
             return View("AddGuest");
         }
 
+        /// <summary>
+        /// Post from add guest page, adds new guest to the db and returns the new list of guests
+        /// </summary>
+        /// <param name="guestViewModel"></param>
+        /// <returns></returns>
         [HttpPost("/AddGuest")]
         public IActionResult AddNewGuest(GuestViewModel guestViewModel)
         {
 
             _weddingDbContext.Guests.Add(guestViewModel.ActiveGuest);
             _weddingDbContext.SaveChanges();
-            return View("GuestList");
+            return RedirectToAction("GetGuestList", "GuestList");
         }
 
+        /// <summary>
+        /// get req for guest list, outputs the list of guests in order by last name
+        /// </summary>
+        /// <returns></returns>
         [HttpGet("/GuestList")]
         public IActionResult GetGuestList()
         {
             List<Guests> guests = _weddingDbContext.Guests.OrderBy(g => g.LastName).ToList();
+            //turn the list into the view model then pass to the cshtml page
             GuestListViewModel guestList = new GuestListViewModel()
             {
                 Guests = guests
             };
 
             return View("GuestList", guestList);
+        }
+
+        /// <summary>
+        /// Get req for edit, takes the guest id and outputs the guest info into a form, can make edits
+        /// </summary>
+        /// <param name="guestId"></param>
+        /// <returns></returns>
+        [HttpGet("/EditGuest/{guestId}")]
+        public IActionResult EditGuestRequest(int guestId)
+        {
+
+            return View();
         }
     }
 }
