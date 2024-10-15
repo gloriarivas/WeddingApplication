8eb256a3adef357df2d255679af243fae178d7ae
Gloria Rivas-Bonilla <Grivas8325@conestogac.on.ca>
2024-10-15 14:16:19 -0400

    images can be displayed, still need to format

--- WeddingApp/Controllers/PictureSlideController.cs
+++ WeddingApp/Controllers/PictureSlideController.cs
@@ -18,13 +18,26 @@ namespace WeddingApp.Controllers
             return View();
         }
 
+        [HttpGet("/Gallery")]
         public IActionResult GetImages() {
+            //TODO: fix this
             List<Pictures> pics = _weddingDbContext.Pictures.ToList();
+            //foreach (var picture in pics) {
+            //    string url = Url.Action("getImage", "PictureSlide", new { path = picture.Url }, Request.Scheme);
+            //    picture.Url = url;
+            //}
             ImageViewModel imageViewModel = new ImageViewModel()
             {
                 Pictures = pics
             };
-            return View(imageViewModel);        
+            return View("PictureSlide",imageViewModel);        
+        }
+
+        private IActionResult getImage(string path)
+        {
+            byte[] imageBytes = System.IO.File.ReadAllBytes(path);
+
+            return File(imageBytes, "image/jpeg");
         }
     }
 }
